6390c7b3d48e2ee816a2ecbf9ddc70a8400e4851
ngtanphat-hcmus <phattannguyen533@gmail.com>
2026-06-23 11:00:58 +0700

    Time: 120 ms (7.8%), Space: 17.2 MB (3.46%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.c
@@ -0,0 +1,13 @@
+double findMaxAverage(int* nums, int numsSize, int k) {
+    int current =0;
+    int max =0;
+    for(int i =0; i<k;i++){
+        current += nums[i];
+        max = current;
+    }
+    for(int i =k;i<numsSize;i++){
+        current = current + nums[i] -nums[i-k];
+        if(current>max) max = current;
+    }
+    return (double)max/k;
+}
