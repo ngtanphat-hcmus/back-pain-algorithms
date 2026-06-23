957e0bd5079cc1432ad6202329af4f9e59091acb
ngtanphat-hcmus <phattannguyen533@gmail.com>
2026-06-23 11:00:02 +0700

    Time: 0 ms (100%), Space: 10.5 MB (39.67%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
+  *returnSize = candiesSize;
+  bool* result = (bool*)malloc(candiesSize*sizeof(bool));
+  int maxCandies = candies[0];
+  for(int i = 1; i<candiesSize; i++){
+    if(candies[i]>=maxCandies){
+        maxCandies = candies[i];
+    }
+  }
+    for(int i = 0; i<candiesSize; i++){
+        if(candies[i] + extraCandies >=maxCandies){
+             result[i] = true;
+        }
+        else result[i] = false;
+    }
+    return result;
+  } 
+    
+    
